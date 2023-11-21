56a1a504ead6ccead5bc152af73a482cbc333df4
Anji babu Nakka <147472806+codeminded-143@users.noreply.github.com>
2023-11-21 20:18:34 +0530

    #include<stdio.h>
    int main()
    {
        int n,i,n1,rev = 0;
        
        scanf("%d",&n);
        
        i=n;
        
        while(n != 0){// 12 != 0 
            
            n1 = n%10;// 121%10 = 1, 12%10 = 2
            rev = (rev*10)+n1;//0*10+1=1 , 1*10+2=12
            n/=10;//121/10 = 12,12/10 = 1 
        }
            
        if(rev == i){
            printf("True");
        }else{
            printf("False");
        }
        
    }

---
+++ Palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,n1,rev = 0;
+    
+    scanf("%d",&n);
+    
+    i=n;
+    
+    while(n != 0){// 12 != 0 
+        
+        n1 = n%10;// 121%10 = 1, 12%10 = 2
+        rev = (rev*10)+n1;//0*10+1=1 , 1*10+2=12
+        n/=10;//121/10 = 12,12/10 = 1 
+    }
+        
+    if(rev == i){
+        printf("True");
+    }else{
+        printf("False");
+    }
+    
+}
