60d480b2591f14d1a5add479eee545a118b013e5
Anji babu Nakka <147472806+codeminded-143@users.noreply.github.com>
2023-12-19 09:56:01 +0530

    #include<stdio.h> int main(){     int n,i,j,ch;     scanf("%d",&n);     ch = 65+(n-1);     for(i = 1; i <= n;i++){
            for(j = 1;j<=i;j++){
                printf("%c ",ch);
            }printf("
    ");
            ch--;
        }
    }

---
+++ Basic_Pattern_2.5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,ch;
+    scanf("%d",&n);
+    ch = 65+(n-1);
+    for(i = 1; i <= n;i++){
+         
+        for(j = 1;j<=i;j++){
+            printf("%c ",ch);
+        }printf("
+");
+        ch--;
+    }
+}
