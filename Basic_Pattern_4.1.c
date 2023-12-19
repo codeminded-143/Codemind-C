a5f58f739e612e0795d2952b9864c1fb6a6f8f82
Anji babu Nakka <147472806+codeminded-143@users.noreply.github.com>
2023-12-19 20:13:58 +0530

    #include<stdio.h> int main(){     int n,i,j,k;     scanf("%d",&n);     for(i = 1;i<=n;i++){         for(j = 1;j<=n;j++){             if(i%2 == 0){                 k = 1;                 printf("%d ",k);             }             else{                 k = 0;                 printf("%d ",k);             }         }printf(" ");     } }

---
+++ Basic_Pattern_4.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i = 1;i<=n;i++){
+        for(j = 1;j<=n;j++){
+            if(i%2 == 0){
+                k = 1;
+                printf("%d ",k);
+            }
+            else{
+                k = 0;
+                printf("%d ",k);
+            }
+        }printf("
+");
+    }
+}
